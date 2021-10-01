318ca112880b28ed646410e2884b03dbae1d437c
TheCringeLord <65627245+TheCringeLord@users.noreply.github.com>
2021-10-01 02:46:13 -0500

    actual final update (probably)
    removed two lines, I highly doubt it can get smaller than this without breaking

--- Mass-Defect.py
+++ Mass-Defect.py
@@ -1,6 +1,4 @@
 Mnucleus = (float(input("Enter Mass Number: ")) * float(1.66054E-27)) - (float(input("Enter Electron Amount: ")) * float(9.10915E-31))
 Mprotons = (float(input("Enter Proton Amount: ")) * float(1.67262E-27)) + (float(input("Enter Neutron Amount: ")) * float(1.67495E-27))
-print("Mnucleus = ", Mnucleus)
-print("Mprotons = ", Mprotons)
-print("Mass Defect = ", Mnucleus - Mprotons)
+print("Mnucleus =", Mnucleus), print("Mprotons =", Mprotons), print("Mass Defect =", Mnucleus - Mprotons)
 input()
